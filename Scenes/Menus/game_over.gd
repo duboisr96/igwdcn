01feb6b0e1ae50eaf2733bfa692f438f24205e98
Rodrigo Gerbasi <rgerbasi98@gmail.com>
2025-02-21 12:46:27 -0500

    game over should be working
    yes

--- Scenes/Menus/game_over.gd
+++ Scenes/Menus/game_over.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 class_name GameOverScreen
 
 var current : PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("game over")
@@ -10,23 +11,28 @@ func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	#print(self.Anchor)
-	
-	
 	$ColorRect.modulate = Color(1,1,1,0)
 	var tween = create_tween()
 	tween.tween_property($ColorRect, "modulate", Color(1,1,1,1), 2.0)
 
 
-func game_over(tree):
-	print(tree)
-	for child in tree.current_scene.get_children():
-		if child is CanvasLayer:
-			print(child.visible)
-			child.add_child(self)
+func game_over():
+	#print(tree)
+	#for child in tree.current_scene.get_children():
+		#if child is CanvasLayer:
+			#print(child.visible)
+			#child.add_child(self)
+	get_tree().paused = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$ColorRect.modulate = Color(1,1,1,0)
+	var tween = create_tween()
+	tween.tween_property($ColorRect, "modulate", Color(1,1,1,1), 2.0)
 
 func _on_retry_pressed():
 	#get_tree().reload_current_scene()
+	queue_free()
+	get_tree().paused = false
+	await get_tree().process_frame
 	get_tree().reload_current_scene()
 
 
