b171448cbe98dd9711feb8aa24ab02a6b67286ff
Rodrigo Gerbasi <rgerbasi98@gmail.com>
2025-02-21 15:26:06 -0500

    Graphic and update for player UI
    its not much but its honest work

--- player_gui.gd
+++ player_gui.gd
@@ -12,7 +12,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Timer.text = timer_display
-	$Quota_todo.text = quota_todo
-	$Quota_done.text = quota_done
-	pass
+	$MarginContainer2/MarginContainer/VBoxContainer/Timer.text = timer_display
+	$MarginContainer2/MarginContainer/VBoxContainer/Quota_done.text = quota_done
+	$MarginContainer2/MarginContainer/VBoxContainer/Quota_todo.text = quota_todo
+	#$MarginContainer2/MarginContainer/VBoxContainer/Test.text = quota_todo
+	#$Timer.text = timer_display
+	#$Quota_todo.text = quota_todo
+	#$Quota_done.text = quota_done
+	
